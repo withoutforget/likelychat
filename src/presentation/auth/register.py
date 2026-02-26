481daa64580676b1e233bee40fb3b24b65aeabe8
quadronik <t.astarckin@gmail.com>
2026-02-26 17:58:12 +0300

    added user registrashion without password hashing

--- src/presentation/auth/register.py
+++ src/presentation/auth/register.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
 
 from dishka.integrations.fastapi import DishkaRoute, FromDishka
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from pydantic import BaseModel
 
 from src.application.auth.auth_service import AuthService
 from src.infra.postgres.repo.user.repository import UserRepository
@@ -9,19 +10,30 @@ from src.infra.postgres.repo.user.repository import UserRepository
 ROUTER = APIRouter(prefix="/register", route_class=DishkaRoute)
 
 
-@dataclass(slots=True)
-class RegisterRequest:
+class RegisterRequest(BaseModel):
     username: str
     password: str
 
 
-@dataclass(slots=True)
-class RegisterResponse:
+
+class RegisterResponse(BaseModel):
     token: str | None = field(default=None)
 
 
 @ROUTER.post("/")
 async def register(
-    payload: RegisterRequest, service: FromDishka[AuthService]
+    payload: RegisterRequest,
+    service: FromDishka[AuthService],
+    rep: FromDishka[UserRepository]
 ) -> RegisterResponse:
-    pass
+        if await rep.exists(payload.username):
+            raise HTTPException(
+                status_code= 400,
+                detail= "user with that username alredy exist")
+        else:
+            await rep.create(payload.username, payload.password)
+            user = await rep.get_by_username(payload.username)
+            user_id = user.id
+            token = service.create_token(user_id, payload.username)
+            return RegisterResponse(token=token)
+            
